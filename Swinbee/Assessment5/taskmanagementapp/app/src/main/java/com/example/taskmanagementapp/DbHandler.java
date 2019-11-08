537ed93685b5ea4d985fad270140fdb9c1baeef2
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-11-08 17:10:33 +0800

    Created TaskObject class and DbHandler class
    TaskObject is to store related data like ID, due date, title, etc
    DbHandler deals with operations to the database.

---
+++ Swinbee/Assessment5/taskmanagementapp/app/src/main/java/com/example/taskmanagementapp/DbHandler.java
@@ -0,0 +1,151 @@
+package com.example.taskmanagementapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class DbHandler extends SQLiteOpenHelper {
+
+    private Context context;
+    private static ArrayList<TaskObject> taskObjectList;
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "taskOject_database", TABLE_NAME = "taskObject_table";
+
+    private static final String KEY_ID = "id", KEY_TITLE = "title", KEY_DUE_DATE = "due_date", KEY_DETAILS = "details", KEY_PRIORITY = "priority", KEY_COMPLETION_STATUS = "completion_status";
+
+    public DbHandler(@Nullable Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String create_table_query = "CREATE TABLE " + TABLE_NAME + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_TITLE + " TEXT, " + KEY_DUE_DATE + " TEXT, " + KEY_DETAILS + " TEXT, " + KEY_PRIORITY + " TEXT, " + KEY_COMPLETION_STATUS + " TEXT)";
+        sqLiteDatabase.execSQL(create_table_query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void addTaskObject(TaskObject object){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_TITLE, object.getTitle());
+        contentValues.put(KEY_DUE_DATE, object.getDue_date());
+        contentValues.put(KEY_DETAILS, object.getDetails());
+        contentValues.put(KEY_PRIORITY, object.getPriority());
+        contentValues.put(KEY_COMPLETION_STATUS, object.getCompletion_status());
+
+        db.insert(TABLE_NAME, null, contentValues);
+        db.close();
+    }
+
+    public void updateTaskObject(TaskObject object){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_TITLE, object.getTitle());
+        contentValues.put(KEY_DUE_DATE, object.getDue_date());
+        contentValues.put(KEY_DETAILS, object.getDetails());
+        contentValues.put(KEY_PRIORITY, object.getPriority());
+        contentValues.put(KEY_COMPLETION_STATUS, object.getCompletion_status());
+
+        db.update(TABLE_NAME, contentValues, KEY_ID + "=" + object.getId(), null);
+        db.close();
+    }
+
+    public void deleteTaskObject(TaskObject object){
+        SQLiteDatabase db = this.getReadableDatabase();
+        db.delete(TABLE_NAME, KEY_ID + "=?", new String[]{String.valueOf(object.getId())});
+    }
+
+    public TaskObject getTaskObject(int objectID){
+        TaskObject object = null;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME, new String[]{KEY_ID, KEY_TITLE, KEY_DUE_DATE, KEY_DETAILS, KEY_PRIORITY, KEY_COMPLETION_STATUS}, KEY_ID + "=?", new String[]{String.valueOf(objectID)}, null, null, null, null);
+
+        if (cursor != null){
+            cursor.moveToFirst();
+            object = new TaskObject(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5));
+            cursor.close();
+        }
+        return object;
+    }
+
+    public ArrayList<TaskObject> getAllTaskObjects(){
+        ArrayList<TaskObject> objectList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_NAME;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()){
+            do {
+                TaskObject object = new TaskObject(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5));
+                objectList.add(object);
+            } while(cursor.moveToNext());
+            cursor.close();
+        }
+        return objectList;
+    }
+
+    public ArrayList<TaskObject> getOverdueTaskObjects(){
+        ArrayList<TaskObject> objectList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_NAME;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()){
+            do{
+                TaskObject object = new TaskObject(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5));
+                try {
+                    Date date = simpleDateFormat.parse(object.getDue_date());
+                    if (new Date().after(date)){
+                        objectList.add(object);
+                    }
+                } catch (ParseException e){
+                    e.printStackTrace();
+                }
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+        return objectList;
+    }
+
+    public ArrayList<TaskObject> getAllPending(){
+        ArrayList<TaskObject> objectList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_NAME;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()){
+            do{
+                TaskObject object = new TaskObject(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5));
+                try {
+                    Date date = simpleDateFormat.parse(object.getDue_date());
+                    if (new Date().before(date)){
+                        objectList.add(object);
+                    }
+                } catch (ParseException e){
+                    e.printStackTrace();
+                }
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+        return objectList;
+    }
+}
---
+++ Swinbee/Assessment5/taskmanagementapp/app/src/main/java/com/example/taskmanagementapp/TaskObject.java
@@ -0,0 +1,88 @@
+package com.example.taskmanagementapp;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TaskObject implements Parcelable {
+    private int id;
+    private String title, due_date, details, priority, completion_status;
+
+    public TaskObject(){};
+
+    public TaskObject(int id, String title, String due_date, String details, String priority, String completion_status) {
+        this.id = id;
+        this.title = title;
+        this.due_date = due_date;
+        this.details = details;
+        this.priority = priority;
+        this.completion_status = completion_status;
+    }
+
+    public TaskObject(String title, String due_date, String details, String priority, String completion_status) {
+        this.title = title;
+        this.due_date = due_date;
+        this.details = details;
+        this.priority = priority;
+        this.completion_status = completion_status;
+    }
+
+    protected TaskObject(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        due_date = in.readString();
+        details = in.readString();
+        priority = in.readString();
+        completion_status = in.readString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDue_date() {
+        return due_date;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public String getCompletion_status() {
+        return completion_status;
+    }
+
+    public static final Creator<TaskObject> CREATOR = new Creator<TaskObject>() {
+        @Override
+        public TaskObject createFromParcel(Parcel in) {
+            return new TaskObject(in);
+        }
+
+        @Override
+        public TaskObject[] newArray(int size) {
+            return new TaskObject[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(title);
+        parcel.writeString(due_date);
+        parcel.writeString(details);
+        parcel.writeString(priority);
+        parcel.writeString(completion_status);
+    }
+}
