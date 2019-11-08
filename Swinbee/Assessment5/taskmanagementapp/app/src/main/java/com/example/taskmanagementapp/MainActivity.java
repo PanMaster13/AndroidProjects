9893df65c1bb8e1587030782d9a0ec320938d4f1
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-11-08 15:59:53 +0800

    Bottom menu navigation completed.
    Only toasting messages, will make true method soon.

--- Swinbee/Assessment5/taskmanagementapp/app/src/main/java/com/example/taskmanagementapp/MainActivity.java
+++ Swinbee/Assessment5/taskmanagementapp/app/src/main/java/com/example/taskmanagementapp/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.taskmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_completed:
+                        Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_overdue:
+                        Toast.makeText(MainActivity.this, "Overdue", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
